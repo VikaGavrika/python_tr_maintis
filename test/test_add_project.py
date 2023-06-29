175b7ef807d7840faa61f05ced20521b7f5b02f1
Veekeetee <gavriviktoriia@gmail.com>
2023-06-29 23:32:31 +0300

    Добавила проверку обычных списков в тест по добавлению проекта в мантис.

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -23,5 +23,6 @@ def test_add_project_db(app, db, project, check_ui):
     new_projects = db.get_project_list()
     assert len(old_projects) + 1 == len(new_projects)
     old_projects.append(project)
+    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
     if check_ui:
         assert sorted(new_projects, key=Project.id_or_max) == sorted(app.project.get_project_list(), key=Project.id_or_max)
